2276b164673e2f633c0644b0bafe31ff49d509c4
Melody Flores <melody03@hotmail.com.ar>
2023-08-22 08:58:31 -0300

    details fixing useEffect bug

--- src/layouts/Details.jsx
+++ src/layouts/Details.jsx
@@ -3,8 +3,8 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom"
 
 const EventDetails = () => {
-    const { id } = useParams(); // ID de mi evento
-    // Tener una variable de estado para almacenar la info de mi evento
+    const { id } = useParams();
+    console.log(id);
 
     useEffect(() => {
         console.log("UseEffect executed");
@@ -16,10 +16,6 @@ const EventDetails = () => {
           .catch(err => console.log(err))
     }, [])
   
-
-    
-
-    // Utilizar esa variable de estado para renderizar la info
     return (
         <div>
             <h2 className='text-3xl text-center'>Detalle del evento: {id}</h2>
